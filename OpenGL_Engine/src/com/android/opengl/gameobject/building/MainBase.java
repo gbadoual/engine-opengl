ec306ca59349d0d5da02bf1fc99463a97568194f
ArtTema2010@gmail.com <ArtTema2010@gmail.com@328ee28e-fa12-2cb2-d335-c80b01a904a3>
2012-08-20 15:55:52 +0000

    Unit constructing experiments

--- OpenGL_Engine/src/com/android/opengl/gameobject/building/MainBase.java
+++ OpenGL_Engine/src/com/android/opengl/gameobject/building/MainBase.java
@@ -1,9 +1,14 @@
 package com.android.opengl.gameobject.building;
 
+import java.util.Random;
+
 import android.util.Log;
 
 import com.android.opengl.R;
 import com.android.opengl.gameobject.Scene;
+import com.android.opengl.gameobject.unit.Cube;
+import com.android.opengl.gameobject.unit.Earth;
+import com.android.opengl.util.geometry.Point3D;
 import com.android.opengl.view.control.GLButton;
 import com.android.opengl.view.control.GLGridLayout;
 import com.android.opengl.view.control.GLView;
@@ -23,12 +28,26 @@ public class MainBase extends AbstractBuilding{
 	}
 
 
+	private Random r = new Random();
 	private void initView() {
 		mainPanleLayout = new GLGridLayout(parentScene, 5, 10, 30, 0);
 		
 				
 		GLView child = new GLButton(parentScene, 50, 20, 30, 10);
 		GLView child1 = new GLButton(parentScene, 5, 2, 10, 10);
+		final float distance = 80;
+		child1.setOnTapListener(new GLView.OnTapListener() {
+			
+			@Override
+			public void onTap(GLView glView) {
+				Log.i("tag", "another glview tapped: " + glView);
+				Earth earth = new Earth(parentScene);
+				float x = r.nextFloat() * distance - distance/2;
+				float z = r.nextFloat() * distance - distance/2;
+				float y = parentScene.getAltitude(x, z);
+				earth.getMovingTool().moveTo(new Point3D(x, y, z));
+			}
+		});
 		GLView child2 = new GLButton(parentScene, 5, 2, 10, 10);
 		GLView child3 = new GLButton(parentScene, 5, 2, 10, 5);
 		GLView child4 = new GLButton(parentScene, 5, 2, 10, 5);
@@ -40,6 +59,11 @@ public class MainBase extends AbstractBuilding{
 			@Override
 			public void onTap(GLView glView) {
 				Log.i("tag", "another glview tapped: " + glView);
+				Cube cube = new Cube(parentScene);
+				float x = r.nextFloat() * distance - distance/2;
+				float z = r.nextFloat() * distance - distance/2;
+				float y = parentScene.getAltitude(x, z);
+				cube.getMovingTool().moveTo(new Point3D(x, y, z));
 			}
 		});
 		mainPanleLayout.addChild(child1);
