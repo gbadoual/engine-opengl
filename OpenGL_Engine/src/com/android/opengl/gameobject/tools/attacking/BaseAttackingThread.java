79d754622e9c7eec4df7fdb2d4213a54b8ef29c5
ArtTema2010@gmail.com <ArtTema2010@gmail.com@328ee28e-fa12-2cb2-d335-c80b01a904a3>
2012-08-22 15:42:25 +0000

    Attacking opportunity implementation

--- OpenGL_Engine/src/com/android/opengl/gameobject/tools/attacking/BaseAttackingThread.java
+++ OpenGL_Engine/src/com/android/opengl/gameobject/tools/attacking/BaseAttackingThread.java
@@ -1,31 +1,55 @@
 package com.android.opengl.gameobject.tools.attacking;
 
+import android.util.Log;
+
 import com.android.opengl.gameobject.GameObject;
+import com.android.opengl.gameobject.tools.moving.BaseMovingThread;
+import com.android.opengl.util.geometry.Point3D;
 import com.android.opengl.util.geometry.Vector3D;
 
 public class BaseAttackingThread extends Thread{
 	
+	private static final String TAG = BaseAttackingThread.class.getSimpleName();
 	private GameObject attackingObject;
 	private GameObject objectToAttack;
 	
-	
+	private Point3D lastApponentPos;
 	
 	
 	public BaseAttackingThread(GameObject attackingObject, GameObject objectToAttack) {
 		this.attackingObject = attackingObject;
 		this.objectToAttack = objectToAttack;
+		this.lastApponentPos = objectToAttack.getPosition();
 	}
 
 	@Override
 	public void run() {
 		
 		while(!isInterrupted()){
-			float distance = getObjectObjectDistance(attackingObject, objectToAttack);
+			float distance = getDistance(attackingObject, objectToAttack);
+			Vector3D destVector = new Vector3D(attackingObject.getPosition(), lastApponentPos);
+			float attackingRadius = attackingObject.getAttackingTool().attackingRadius;
+			float len = destVector.getLength() - attackingRadius;
+			
+			attackingObject.getMovingTool().beginFollowingObject(objectToAttack, attackingRadius, 0);
+
+			while(attackingObject.getMovingTool().isMoving()){
+				if(objectToAttack.getMovingTool().isMoving()){
+					
+				}
+			}
+//			if(distance > attackingRadius){
+//				followObjectToAttack();
+//				if(isInterrupted()){
+//					return;
+//				}
+//			}
 			
 		}
 	}
 	
-	private float getObjectObjectDistance(GameObject first, GameObject second){
+
+	private float getDistance(GameObject first, GameObject second){
 		Vector3D distanceVector = new Vector3D(first.getPosition(), second.getPosition()).normalize();
 		return distanceVector.getLength();
 	}
