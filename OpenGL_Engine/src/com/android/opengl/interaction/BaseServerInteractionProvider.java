4b7d5411edc1cfe64de9db45fd8cc4f22e28080e
ArtTema2010 <ArtTema2010@gmail.com>
2013-02-03 19:12:49 +0000

    Added main screen. User object implementation

--- OpenGL_Engine/src/com/android/opengl/interaction/BaseServerInteractionProvider.java
+++
@@ -1,9 +0,0 @@
-package com.android.opengl.interaction;
-
-
-public interface BaseServerInteractionProvider extends BaseInteractionProvider{
-
-	public void startServer();
-	public void stopServer();
-	
-}
