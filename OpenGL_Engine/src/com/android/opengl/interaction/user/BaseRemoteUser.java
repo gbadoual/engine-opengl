5e8b77a659e52b44828386b22d15aef95a3f7fbc
ArtTema2010 <ArtTema2010@gmail.com>
2013-02-03 19:10:41 +0000

    Added main screen. User object implementation

---
+++ OpenGL_Engine/src/com/android/opengl/interaction/user/BaseRemoteUser.java
@@ -0,0 +1,13 @@
+package com.android.opengl.interaction.user;
+
+import com.android.opengl.gameobject.GLScene;
+
+
+public abstract class BaseRemoteUser extends BaseUser{
+
+	public BaseRemoteUser(GLScene scene) {
+		super(scene);
+	}
+	
+
+}
