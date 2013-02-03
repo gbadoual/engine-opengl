47de012d3bd027e195fe53ae0ae07750805e3bba
ArtTema2010 <ArtTema2010@gmail.com>
2013-02-03 19:37:44 +0000

    Added main screen. User object implementation

---
+++ OpenGL_Engine/src/com/android/opengl/interaction/remote/IBaseClient.java
@@ -0,0 +1,9 @@
+package com.android.opengl.interaction.remote;
+
+
+public interface IBaseClient extends IBaseInteractionProvider{
+
+	public void startClient();
+	public void stopClient();
+	
+}
---
+++ OpenGL_Engine/src/com/android/opengl/interaction/remote/bluetooth/BluetoothServer.java
@@ -0,0 +1,120 @@
+package com.android.opengl.interaction.remote.bluetooth;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.json.JSONObject;
+
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+
+import com.android.opengl.interaction.remote.IBaseServer;
+import com.android.opengl.util.Log;
+
+public class BluetoothServer extends BaseBluetoothInteractionProvider implements IBaseServer{
+	
+	private static final String TAG = BluetoothServer.class.getSimpleName();
+	private List<BluetoothSocket> mBluetoothClientSocketList = Collections.synchronizedList(new ArrayList<BluetoothSocket>());
+	
+	private BluetoothServerThread mServerThread;
+
+	public BluetoothServer() throws IllegalAccessException{
+		super(TAG);
+	}
+	
+	private class BluetoothServerThread extends Thread{
+		
+		private BluetoothServerSocket mBluetoothServerSocket;
+		private String mName;
+		private UUID mUuid;
+		
+		
+		public BluetoothServerThread(String name, UUID uuid) throws IOException {
+			mName = name;
+			mUuid = uuid;
+		}
+		
+		@Override
+		public void run() {
+			while(!isInterrupted()){
+				try {
+					mBluetoothServerSocket = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(mName, mUuid);
+					BluetoothSocket bluetoothSocket = mBluetoothServerSocket.accept();
+					mBluetoothClientSocketList.add(bluetoothSocket);
+					notifyDeviceConnected(bluetoothSocket);
+					closeServerSocket(mBluetoothServerSocket);
+					
+				} catch (IOException e) {
+					Log.e(TAG, "run(): closing server socket" + e);
+				}finally{
+					closeServerSocket(mBluetoothServerSocket);
+				} 
+			}
+			closeServerSocket(mBluetoothServerSocket);
+		}
+
+		private void closeServerSocket(BluetoothServerSocket mBluetoothServerSocket) {
+			try {
+				if(mBluetoothServerSocket != null){
+					mBluetoothServerSocket.close();
+				}
+			} catch (IOException e) {
+				Log.e(TAG, "closeServerSocket(): " + e);				
+			}
+		}
+	}
+
+	@Override
+	public void sendData(JSONObject data) {
+		for(BluetoothSocket bluetoothClientSocket: mBluetoothClientSocketList){
+			try {
+				OutputStream outputStream = bluetoothClientSocket.getOutputStream();
+				PrintWriter printWriter = new PrintWriter(outputStream);
+				printWriter.println(data.toString());
+				printWriter.flush();
+			} catch (IOException e) {
+				Log.e(TAG, "sendData(): " + e);
+			}
+		}
+	}
+
+	public void startServer(String name, UUID uuid) throws IOException{
+		if(mServerThread != null){
+			throw new IllegalStateException("Serevr is already started. Use stopServer() before starting it again");
+		}
+		mServerThread = new BluetoothServerThread(name, uuid);
+		mServerThread.start();
+		resgisterOnBTDeviceConnectListener(mBluetoothDeviceConnectListener);
+		startListningData(mBluetoothClientSocketList);
+	}
+	
+	public void stopServer(){
+		if(mServerThread != null){
+			mServerThread.interrupt();
+		}
+		mServerThread = null;
+		stopListningData();
+		unresgisterOnBTDeviceConnectListener(mBluetoothDeviceConnectListener);
+	}
+	
+	@Override
+	public void startServer() {
+		// TODO Auto-generated method stub
+	}
+	
+	private OnBluetoothDeviceConnectListener mBluetoothDeviceConnectListener = new OnBluetoothDeviceConnectListener() {
+		
+		@Override
+		public void onBluetoothDeviceConnected(BluetoothSocket bluetoothSocket) {
+			stopListningData();
+			startListningData(mBluetoothClientSocketList);
+		}
+	};
+
+
+}
